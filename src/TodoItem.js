99e31c7fea0ba558efcb8bb261688d5e12edb745
邱雨顺 <1213524765@qq.com>
2021-11-19 16:45:20 +0800

    study02

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -8,7 +8,32 @@ class TodoItem extends React.Component(){
     }
     render(){
         <div className="content">
-
+                      <h1>未完成<span id="todocount">{this.state.list.length}</span></h1>
+          <ol id="todolist">
+            {this.state.list.filter(item=>(item.flag === 0)).map((item,index)=>{
+              return<li key={index}>
+                    <input type="checkbox" checked={false} onClick={(e)=>{this.handleCheck(e,item,index)}}></input>
+                    <p>{item.content}</p>
+                    <a  onClick={this.handleDelete.bind(this,index)}
+                        href= "###"
+                    >
+                    </a>
+              </li>
+            })}
+          </ol>
+          <h1>已经完成<span id="donecount">{this.state.donelist.length}</span></h1>
+          <ol id="donelist">
+            {this.state.donelist.map((item,index)=>{
+              return<li key={index}>
+                    <input type="checkbox" checked={true} onClick={(e)=>{this.handleCheck2(e,item,index)}}></input>
+                    <p>{item.content}</p>
+                    <a  onClick={this.handleDelete2.bind(this,index)}
+                        href= "###"
+                    >
+                    </a>
+              </li>
+            })}
+          </ol>
         </div>
     }
 }
